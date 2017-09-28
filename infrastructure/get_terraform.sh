a953f4401caffed5cb3565a0332c9aadcb762dad
Brian Peterson <brian@epetersons.com>
2017-09-28 01:59:02 +0000

    fixed travis enc line

--- infrastructure/get_terraform.sh
+++
@@ -1,3 +0,0 @@
-TERRAFORM_VERSION=0.10.6
-curl -s https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip | gzip - -d > ./terraform
-chmod 755 ./terraform
