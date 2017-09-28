86183c3ed9b414c42475e036f445d81edbde02ef
Brian Peterson <brian@epetersons.com>
2017-09-28 00:41:04 +0000

    working on travis build/push

---
+++ create_ecr_secrets.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# This script uses the aws cli to get the ecr login information
+# and stores it in the ecr.secrets file locally
+
+OUTFILE=ecr.secrets
+# TODO: make this dynamic...
+REGION='us=west-2'
+
+echo "Creating ecs.secret file"
+# This is the command that gives us docker login command w/ credentials
+# Note, must be authenticated to aws for this command
+ECR_CMD=`aws ecr get-login --no-include-email --region ${REGION}`
+# Run ecr command, then extract the user name of the repo, store in secrets file
+echo "ECR_USER=`${ECR_CMD} | awk '{print$4}'`" > ${OUTFILE}
+# Run ecr command, then extract the password for the repo, store in secrets file
+echo "ECR_PASS=`${ECR_CMD} | awk '{print$6}'`" >> ${OUTFILE}
+# Run ecr command, then extract the url of the repo, store in secrets file
+echo "ECR_URL=`${ECR_CMD}  | awk '{print$7}'`" >> ${OUTFILE}
+# Run ecr command, then extract the dns name for the repo, store in secrets file
+echo "ECR_DNS=`${ECR_CMD}  | awk '{print$7}' | sed s/'https:\/\/'//`" >> ${OUTFILE}
+
+
+echo "encrypting the ${OUTFILE} file"
+# Now, encrypt the ecs.secrets file into ecs.secretes.enc and add decoding
+# instructions to the .travis.yml file.
+# Note, must be logged into travis for this step
+travis encrypt-file ${OUTFILE} --add
--- docker_push.sh
+++ docker_push.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-#set -x
-if [ -f ./ecr.secret ]; then
-  . ./ecr.secret
-fi
+# We want this script to exit if we get any errors
+set -o
+# This script pushes the built docker image to the repository
 
+# We should have secrets file created by create_ecr_secrets.sh, encoded
+# in the dev environment and decoded in the travis environment
+
+# Import the AWS ECR variables
+. ./ecr.secret
+
+# Log into the AWS ECR repository
 docker login -u ${ECR_USER} -p ${ECR_PASS} ${ECR_URL}
+# Tag the image we built
+# TODO: get a tagged version number from github, instead of "latest"
 docker tag flask_docker_lab:latest ${ECR_DNS}/flask_docker_lab:latest
+# Now push the docker image to AWS ECR repository
 docker push ${ECR_DNS}/flask_docker_lab_ecr:latest
