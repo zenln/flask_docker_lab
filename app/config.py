3342af7e6908d89fae47a88dd4c14c7353f43235
Brian Peterson <brian@petersons.com>
2017-12-07 16:10:40 -0600

    re-baselined config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 import os
 
 # Title of the site
-SITE_TITLE = 'Joe Flask Docker Lab'
+SITE_TITLE = 'My Flask Docker Lab'
 # The URL for static files/images
 STATIC_URL = 'https://s3-us-west-2.amazonaws.com/flaskdockerlab-static/'
 # The name of the
